3e8d89498f846893f8f2501ce61e05172031b3d0
Your Name <you@example.com>
2022-11-30 16:25:53 -0700

    11-30-2022

--- scripts/app.js
+++ scripts/app.js
@@ -15,13 +15,21 @@ class Player {
 	/**
 	 * @param {number} x
 	 * @param {number} y
+	 * @param {CanvasRenderingContext2D} ctx 
 	 */
 	constructor(x, y) {
 		this.x = x;
 		this.y = y;
 		this.w = game.gridSize;
 		this.h = this.w;
-		this.segments = [];
+		this.ctx =ctx 
+		this.segments = [ new Segment(5* game.gridSize, 5 * game.gridSize, "yellow", this.ctx)];
+			
+	}
+
+	update(){} 
+	draw() {
+		this.segments.foreach 
 	}
 }
 
@@ -30,16 +38,36 @@ class Segment {
 	 * @param {number} x
 	 * @param {number} y
 	 * @param {string} color
+	 * @param {CanvasRenderingContext2D} ctx
 	 */
-	constructor(x, y, color) {
+	constructor(x, y, color, ctx) {
 		this.x = x;
 		this.y = y;
 		this.w = game.gridSize;
 		this.h = this.w;
 		this.color = color;
+		this.ctx = ctx; 
 	}
 
 	update() {}
 
-	draw() {}
+	draw(){
+		this.ctx.fillStyle = this.color;
+		this.ctx.fillRect(this.x, this.y, this.w, this.h);
+	} 
 }
+
+let p1 = new Player 
+
+
+let currentTime = 0; 
+
+function gameloop(timestamp){
+	let elespsedTime = timestamp - currentTime;
+	currentTime = timestamp;
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+	requestAnimationFrame(gameloop);
+}
+
+requestAnimationFrame(gameloop);
